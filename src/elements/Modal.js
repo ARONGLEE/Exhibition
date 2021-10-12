4337ad63a5c862e1cc49dead196901342a00e2f1
Jaehwan Choi <lieblichoi@gmail.com>
2021-10-12 21:34:20 +0900

    Create Image.js

---
+++ src/elements/Modal.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Work from '../components/Work'
+import Image from './Image'
+import '../shared/Modal.scss'
+
+const Modal = ({modalClose}, props) => {
+
+    const onCloseModal = (e) => {
+        console.log('e.target: ', e.target)
+        console.log('e.tarcurrentTargetget: ', e.currentTarget)
+        if(e.target === e.currentTarget){
+            modalClose()
+        }
+
+    }
+    return (
+        <div className="modal__container" onClick={onCloseModal}>
+            <div className="modal">
+                <Image shape= "rectangle" src={props.src}/>
+                <button className="modal__button" onClick={modalClose}> Modal Close</button>
+            </div>
+        </div>
+    )
+}
+
+export default Modal
