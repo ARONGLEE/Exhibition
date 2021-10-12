432188e29b6894bb94c442e85c478ba1be07b995
Jaehwan Choi <lieblichoi@gmail.com>
2021-10-12 21:44:14 +0900

    Create js file
    Modal.js
    ModalContainer.js
    Work.js
    WorkDetail.js
    WorkList.js
    Modal.scss

---
+++ src/components/Work.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+import Grid from "../elements/Grid";
+import Image from "../elements/Image";
+import Modal from "../elements/Modal";
+import ModalContainer from "./ModalContainer";
+const Work = (props) => {
+
+    return (
+      <React.Fragment>
+        <Grid grid>
+          <Image shape= "rectangle" src={props.src}/>
+          <Image shape= "rectangle" src={props.src}/>
+          <Image shape= "rectangle" src={props.src}/>
+          <Image shape= "rectangle" src={props.src}/>
+          <Image shape= "rectangle" src={props.src}/>
+          <Image shape= "rectangle" src={props.src}/>
+          <Image shape= "rectangle" src={props.src}/>
+          <Image shape= "rectangle" src={props.src}/>
+          <Image shape= "rectangle" src={props.src}/>
+        </Grid>
+        <ModalContainer/>
+      </React.Fragment>
+    );
+}
+
+Work.defaultProps = {
+    image_url: "https://mblogthumb-phinf.pstatic.net/20160316_188/machsz_1458108041064UOBNl_JPEG/22.jpg?type=w2",
+};
+
+export default Work;
